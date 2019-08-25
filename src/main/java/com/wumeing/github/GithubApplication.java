72e931faef0a4e2c079da1d17af9140c9dbe3aeb
Luther-st <1054043650@qq.com>
2019-08-25 13:50:25 +0800

    提交成功

--- src/main/java/com/wumeing/github/GithubApplication.java
+++ src/main/java/com/wumeing/github/GithubApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GithubApplication {
-
+	//启动命令
 	public static void main(String[] args) {
+
 		SpringApplication.run(GithubApplication.class, args);
 	}
 
